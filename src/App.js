fdc8f093fe881ff8ab1c9313a7f45311109d0a3a
Sornya 2 <sornya2@Users-MacBook-Air.local>
2021-09-23 11:48:13 -0400

    deploy to github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Form from "./Form";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Form />
+      </div>
+    );
+  }
 }
 
 export default App;
+
+/**
+ <h1> Traditional Uncontrolled form</h1>
+ <form id="myForm">
+  <input name="username" type="text"/>
+  <button>Click Me</button>
+  </form>
+ */
+/** js
+  $("#myForm").submit(function(e){
+    e.preventDefault():
+    $('input[name="usrname"]')  //grab data that we need from input then do something with it
+    alert("you enter: ${username}")
+
+  })
+ */
+/** in react has technique to get data from form this is  controlled components
+   Controlled Components
+   * in HTML, form elements such as <input>, <textarea> ans <select>
+   typically maintain their own state and update it based on ser input
+   * in React, mutable state is kept in the state of components,
+   and omly updated with setState()
+ */
